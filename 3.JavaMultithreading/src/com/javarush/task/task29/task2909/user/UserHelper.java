221ef527acb5757e36aabd88654410e836ceaa5f
lav <Nitlenav@gmail.com>
2017-09-05 16:46:16 +0300

    Остановился на: 6. Перепиши метод calculateRate() класса UserHelper, чтобы метод не изменял входные параметры, а просто возвращал рассчитанное значение (метод должен возвращать int).

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/UserHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/UserHelper.java
@@ -10,36 +10,16 @@ public class UserHelper {
     private boolean isManRoma = true;
 
     public void printUsers() {
-        System.out.println("Имя: " + userAnya.getName());
-        System.out.println("Фамилия: " + userAnya.getSurname());
-        printAdditionalInfo(userAnya);
+        userAnya.printInfo();
+        userAnya.printAdditionalInfo(userAnya);
 
-        System.out.println("Имя: " + userRoma.getName());
-        System.out.println("Фамилия: " + userRoma.getSurname());
-        printAdditionalInfo(userRoma);
-    }
-
-    public void printAdditionalInfo(User user) {
-        if (ageLessThan16(user))
-            System.out.println("Пользователь моложе 16 лет");
-        else
-            System.out.println("Пользователь старше 16 лет");
-    }
-
-    private boolean ageLessThan16(User user) {
-        if (user.getAge() < 16) {
-            return true;
-        }
-        return false;
+        userRoma.printInfo();
+        userRoma.printAdditionalInfo(userRoma);
     }
 
     public int calculateAverageAge() {
-        int age = 28;
-        User userUra = new User("Юра", "Карп", age);
-
-        age = (userAnya.getAge() + userRoma.getAge() + userUra.getAge()) / 3;
-
-        return age;
+        User userUra = new User("Юра", "Карп", 28);
+        return (userAnya.getAge() + userRoma.getAge() + userUra.getAge()) / 3;
     }
 
     public void calculateRate(AtomicInteger base, int age, boolean hasWork, boolean hasHouse) {
