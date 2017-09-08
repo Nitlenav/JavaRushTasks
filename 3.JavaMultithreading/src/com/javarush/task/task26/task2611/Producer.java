4558be5562bf4a560cffe7c07afab8b9f35943eb
lav <Nitlenav@gmail.com>
2017-09-08 18:43:56 +0300

    Задание task26.task2612 №3 решено.

--- 3.JavaMultithreading/src/com/javarush/task/task26/task2611/Producer.java
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2611/Producer.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task26.task2611;
 
+import java.util.Timer;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Producer implements Runnable {
@@ -7,10 +8,17 @@ public class Producer implements Runnable {
         this.map = map;
     }
 
-    private ConcurrentHashMap map;
+    private ConcurrentHashMap <String, String> map = new ConcurrentHashMap<String, String>();
 
     @Override
     public void run() {
-
+        try {
+            Thread.sleep(500);
+            int i = 1;
+            map.put(String.valueOf(i), "Some text for " + i++);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.println(Thread.currentThread().getName() + " thread was terminated");
+        }
     }
 }
