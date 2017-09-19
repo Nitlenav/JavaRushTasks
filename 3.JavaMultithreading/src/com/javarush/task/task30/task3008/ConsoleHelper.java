d131e7594e351f1361cd0504473e465c34e10c06
Александр <Nitlenav@gmail.com>
2017-09-19 22:48:49 +0300

    Из нового задания 7 начал.

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -6,38 +6,31 @@ import java.io.InputStreamReader;
 
 public class ConsoleHelper {
 
-    private static BufferedReader bufferedReader;
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void writeMessage(String message){
         System.out.println(message);
-
     }
 
-    public String readString(){
-        String s;
+    public static String readString(){
         while (true){
             try {
-                s = new BufferedReader(new InputStreamReader(System.in)).readLine();
-                break;
+                return bufferedReader.readLine();
             }
             catch (IOException e){
-                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+                writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
             }
         }
-        return s;
     }
 
-    public int readInt(){
-        int i;
+    public static int readInt(){
         while (true){
             try {
-                i = Integer.parseInt(readString());
-                break;
+                return Integer.parseInt(readString());
             }
             catch (NumberFormatException e){
-                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+                writeMessage("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
             }
         }
-        return i;
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,8 +1,32 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+        Socket socket = null;
+        int port = ConsoleHelper.readInt();
+        try(ServerSocket serverSocket = new ServerSocket(port);) {
+            System.out.println("сервер запущен");
+            while (true){
+                socket = serverSocket.accept();
+               new Handler(socket).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            socket.close();
+        }
+    }
+
+    private static class Handler extends Thread{
+        private Socket socket;
+        public Handler (Socket socket){
+            this.socket = socket;
+        }
     }
 
 }
