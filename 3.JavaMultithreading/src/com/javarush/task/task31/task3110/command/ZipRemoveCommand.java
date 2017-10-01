0aabb859e8935d0f00ccb2c9434b1cacd33f9aa6
Александр <Nitlenav@gmail.com>
2017-10-01 22:20:41 +0300

    task31.task3110 решено полностью.

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
@@ -1,8 +1,19 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipRemoveCommand extends ZipCommand {
+
     @Override
     public void execute() throws Exception {
-        
+        ZipFileManager zipFileManager = getZipFileManager();
+        ConsoleHelper.writeMessage("Из какого архива и какой файл будем удалять?");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        zipFileManager.removeFile(zipPath);
+
     }
 }
